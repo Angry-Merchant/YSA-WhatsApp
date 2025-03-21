83544a4f8db71e60cd89ad7e6de49b60f059d685
Angry Merchant <merchantangry@gmail.com>
2025-03-21 03:45:43 -0400

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,12 +1,12 @@
 const settings = {
   packname: 'Knight',
   author: 'Bot',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  botName: "YSA Assistant",
+  botOwner: 'Alessio', // Your name
+  ownerNumber: '9412965789', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
-  description: "This is a bot for managing group commands and automating tasks.",
+  description: "Beep Boop 0101010101010",
   version: "1.0.0",
 };
 
